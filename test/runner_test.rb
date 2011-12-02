0736d8c7f40e4d53afb72eaa83c06af44d28b389
Nathan Esquenazi <nesquena@gmail.com>
2011-12-02 01:11:01 -0800

    Bump tests sleep to 3 for my slow home computer

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -4,7 +4,7 @@ describe "gitdocs runner" do
   it "should clone files" do
     with_clones(3) do |clone1, clone2, clone3|
       File.open(File.join(clone1, "test"), 'w') { |f| f << "testing" }
-      sleep 2
+      sleep 3
       assert_equal "testing", File.read(File.join(clone1, "test"))
       assert_equal "testing", File.read(File.join(clone2, "test"))
       assert_equal "testing", File.read(File.join(clone3, "test"))
@@ -14,10 +14,10 @@ describe "gitdocs runner" do
   it "should resolve conflicts files" do
     with_clones(3) do |clone1, clone2, clone3|
       File.open(File.join(clone1, "test.txt"), 'w') { |f| f << "testing" }
-      sleep 2
+      sleep 3
       File.open(File.join(clone1, "test.txt"), 'w') { |f| f << "testing\n1" }
       File.open(File.join(clone2, "test.txt"), 'w') { |f| f << "testing\n2" }
-      sleep 2
+      sleep 3
       assert_includes 2..3, Dir[File.join(clone2, "*.txt")].to_a.size
       assert_includes 2..3, Dir[File.join(clone3, "*.txt")].to_a.size
     end
