d4df7e60f5ab18b257de0ef1aec0d06f3477afe1
Andrew Sullivan Cant <acant@alumni.uwaterloo.ca>
2014-06-23 23:43:18 -0400

    Code style updates in Server

--- lib/gitdocs/server.rb
+++ lib/gitdocs/server.rb
@@ -1,5 +1,8 @@
 # -*- encoding : utf-8 -*-
 
+# Disable style checks that are invalid for Renee
+# rubocop:disable Blocks, MultilineBlockChain
+
 require 'thin'
 require 'renee'
 require 'coderay'
@@ -14,6 +17,7 @@ module Gitdocs
       @manager      = manager
       @port         = port.to_i
       @repositories = repositories
+      @search       = Gitdocs::Search.new(repositories)
     end
 
     def self.start_and_wait(manager, override_port, repositories)
@@ -31,18 +35,26 @@ module Gitdocs
       manager      = @manager
       Thin::Logging.debug = @manager.debug
       Thin::Server.start('127.0.0.1', @port) do
-        use Rack::Static, urls: ['/css', '/js', '/img', '/doc'], root: File.expand_path('../public', __FILE__)
+        use Rack::Static, urls: %w(/css /js /img /doc), root: File.expand_path('../public', __FILE__)
         use Rack::MethodOverride
         run Renee {
           if request.path_info == '/'
             if manager.shares.size == 1
               redirect! '/0'
             else
-              render! 'home', layout: 'app', locals: { shares: manager.shares, nav_state: 'home' }
+              render!(
+                'home',
+                layout: 'app',
+                locals: { shares: manager.shares, nav_state: 'home' }
+              )
             end
           else
             path 'settings' do
-              get.render! 'settings', layout: 'app', locals: { conf: manager, nav_state: 'settings' }
+              get.render!(
+                'settings',
+                layout: 'app',
+                locals: { conf: manager, nav_state: 'settings' }
+              )
               post do
                 manager.update_all(request.POST)
                 redirect! '/settings'
@@ -50,13 +62,17 @@ module Gitdocs
             end
 
             path('search').get do
-              render! 'search', layout: 'app', locals: { results: Gitdocs::Search.new(repositories).search(request.GET['q']), nav_state: nil }
+              render!(
+                'search',
+                layout: 'app',
+                locals: { results: @search.search(request.GET['q']), nav_state: nil }
+              )
             end
 
             path('shares') do
               post do
                 Configuration::Share.create
-                redirect! '/settings'
+                redirect!('/settings')
               end
 
               var(:int) do |id|
@@ -69,78 +85,115 @@ module Gitdocs
 
             var :int do |idx|
               repository = repositories[idx]
-
-              halt 404 if repository.nil?
-              file_path = URI.unescape(request.path_info)
+              halt(404) if repository.nil?
+              file_path     = URI.unescape(request.path_info)
               expanded_path = File.expand_path(".#{file_path}", repository.root)
               message_file = File.expand_path('.gitmessage~', repository.root)
-              halt 400 unless expanded_path[/^#{Regexp.quote(repository.root)}/]
+              halt(400) unless expanded_path[/^#{Regexp.quote(repository.root)}/]
               parent = File.dirname(file_path)
               parent = '' if parent == '/'
               parent = nil if parent == '.'
-              locals = { idx: idx, parent: parent, root: repository.root, file_path: expanded_path, nav_state: nil }
-              mime = File.mime_type?(File.open(expanded_path)) if File.file?(expanded_path)
-              mode = request.params['mode']
+              mime   = File.mime_type?(File.open(expanded_path)) if File.file?(expanded_path)
+              mode   = request.params['mode']
+              default_locals = {
+                idx:       idx,
+                parent:    parent,
+                root:      repository.root,
+                file_path: expanded_path,
+                nav_state: nil
+              }
+
               if mode == 'meta' # Meta
                 halt 200, { 'Content-Type' => 'application/json' }, [repository.file_meta(file_path).to_json]
               elsif mode == 'save' # Saving
                 File.open(expanded_path, 'w') { |f| f.print request.params['data'] }
                 File.open(message_file, 'w') { |f| f.print request.params['message'] } unless request.params['message'] == ''
-                redirect! '/' + idx.to_s + file_path
+                redirect!("/#{idx}#{file_path}")
               elsif mode == 'upload'  # Uploading
-                halt 404 unless file = request.params['file']
-                tempfile, filename = file[:tempfile], file[:filename]
+                file = request.params['file']
+                halt 404 unless file
+                tempfile = file[:tempfile]
+                filename = file[:filename]
                 FileUtils.mv(tempfile.path, File.expand_path(filename, expanded_path))
-                redirect! '/' + idx.to_s + file_path + '/' + filename
+                redirect!("/#{idx}#{file_path}/#{filename}")
               elsif !File.exist?(expanded_path) && !request.params['dir'] # edit for non-existent file
                 FileUtils.mkdir_p(File.dirname(expanded_path))
                 FileUtils.touch(expanded_path)
-                redirect!  '/' + idx.to_s + file_path + '?mode=edit'
+                redirect!("/#{idx}#{file_path}?mode=edit")
               elsif !File.exist?(expanded_path) && request.params['dir'] # create directory
                 FileUtils.mkdir_p(expanded_path)
-                redirect!  '/' + idx.to_s + file_path
+                redirect!("/#{idx}#{file_path}")
               elsif File.directory?(expanded_path) # list directory
                 contents =  Dir[File.join(expanded_path, '*')].map { |x| Docfile.new(x) }
-                rendered_readme = nil
-                if readme = Dir[File.expand_path('README.{md}', expanded_path)].first
-                  rendered_readme = '<h3>' + File.basename(readme) + '</h3><div class="tilt">' + render(readme) + '</div>'
-                end
-                render! 'dir', layout: 'app', locals: locals.merge(contents: contents, rendered_readme: rendered_readme)
+                readme = Dir[File.expand_path('README.{md}', expanded_path)].first
+                rendered_readme =
+                  if readme
+                    <<-EOS.gusb(/^\s+/, '')
+                      <h3>#{File.basename(readme)}</h3>
+                      <div class="tilt">#{render(readme)}</div>
+                    EOS
+                  else
+                    nil
+                  end
+                render!(
+                  'dir',
+                  layout: 'app',
+                  locals: default_locals.merge(
+                    contents: contents, rendered_readme: rendered_readme
+                  )
+                )
               elsif mode == 'revisions' # list revisions
                 revisions = repository.file_revisions(file_path)
-                render! 'revisions', layout: 'app', locals: locals.merge(revisions: revisions)
+                render!(
+                  'revisions',
+                  layout: 'app',
+                  locals: default_locals.merge(revisions: revisions)
+                )
               elsif mode == 'revert' # revert file
                 if revision = request.params['revision']
                   File.open(message_file, 'w') { |f| f.print "Reverting '#{file_path}' to #{revision}" }
                   repository.file_revert(file_path, revision)
                 end
-                redirect! '/' + idx.to_s + file_path
+                redirect!("/#{idx}#{file_path}")
               elsif mode == 'delete' # delete file
                 FileUtils.rm(expanded_path)
-                redirect! '/' + idx.to_s + parent
-              elsif mode == 'edit' && (mime.match(%r{text/}) || mime.match(%r{x-empty})) # edit file
+                redirect!("/#{idx}#{parent}")
+              elsif mode == 'edit' && mime.match(/text\/|x-empty/) # edit file
                 contents = File.read(expanded_path)
-                render! 'edit', layout: 'app', locals: locals.merge(contents: contents)
+                render!(
+                  'edit',
+                  layout: 'app',
+                  locals: default_locals.merge(contents: contents)
+                )
               elsif mode != 'raw' # render file
                 revision = request.params['revision']
                 expanded_path = repository.file_revision_at(file_path, revision) if revision
                 begin # attempting to render file
-                  contents = '<div class="tilt">' + render(expanded_path) + '</div>'
+                  contents = %(<div class="tilt">#{render(expanded_path)}</div>)
                 rescue RuntimeError # not tilt supported
-                  contents = if mime.match(%r{text/})
-                    '<pre class="CodeRay">' + CodeRay.scan_file(expanded_path).encode(:html) + '</pre>'
-                  else
-                    %|<embed class="inline-file" src="/#{idx}#{request.path_info}?mode=raw"></embed>|
-                  end
+                  contents =
+                    if mime.match(/text\//)
+                      <<-EOS.gsub(/^\s+/, '')
+                        <pre class="CodeRay">
+                          #{CodeRay.scan_file(expanded_path).encode(:html)}
+                        </pre>
+                      EOS
+                    else
+                      %(<embed class="inline-file" src="/#{idx}#{request.path_info}?mode=raw"></embed>)
+                    end
                 end
-                render! 'file', layout: 'app', locals: locals.merge(contents: contents)
+                render!(
+                  'file',
+                  layout: 'app',
+                  locals: default_locals.merge(contents: contents)
+                )
               else # other file
                 run! Rack::File.new(repository.root)
               end
             end
           end
         }.setup {
-          views_path File.expand_path('../views', __FILE__)
+          views_path(File.expand_path('../views', __FILE__))
         }
       end
     end
