269681e5b160a54562c6ad137c2e0083aacd051d
Andrew Sullivan Cant <acant@alumni.uwaterloo.ca>
2016-02-24 01:31:52 -0500

    Convert to use Celluloid
    * drop the dependencies on event_machine and guard
    * remove Runner and Server which are no longer needed
    * add a Synchronizer class which is a Celluloid actor that does most of
      the work

--- lib/gitdocs.rb
+++ lib/gitdocs.rb
@@ -4,46 +4,11 @@ require 'gitdocs/version'
 require 'gitdocs/initializer'
 require 'gitdocs/share'
 require 'gitdocs/configuration'
-require 'gitdocs/runner'
-require 'gitdocs/server'
 require 'gitdocs/cli'
-require 'gitdocs/manager'
-require 'gitdocs/notifier'
-require 'gitdocs/git_notifier'
 require 'gitdocs/repository'
 require 'gitdocs/search'
 
 module Gitdocs
-  # @param [nil, Integer] override_web_port
-  #
-  # @return (see Gitdocs::Manager#start)
-  def self.start(override_web_port)
-    # assert(@manager.nil?)
-
-    @manager = Manager.new
-    @manager.start(override_web_port)
-  end
-
-  # @return (see Gitdocs::Manager#restart)
-  def self.restart
-    @manager.restart
-  end
-
-  # @return [Logger]
-  def self.logger
-    return @logger if @logger
-
-    output =
-      if Initializer.foreground
-        STDOUT
-      else
-        log_path
-      end
-    @logger = Logger.new(output)
-    @logger.level = Initializer.verbose ? Logger::DEBUG : Logger::INFO
-    @logger
-  end
-
   # @return [String]
   def self.log_path
     File.expand_path('log', Initializer.root_dirname)
@@ -52,24 +17,24 @@ module Gitdocs
   # @param [String] message
   # @return [void]
   def self.log_debug(message)
-    logger.debug(message)
+    Celluloid.logger.debug(message)
   end
 
   # @param [String] message
   # @return [void]
   def self.log_info(message)
-    logger.info(message)
+    Celluloid.logger.info(message)
   end
 
   # @param [String] message
   # @return [void]
   def self.log_warn(message)
-    logger.warn(message)
+    Celluloid.logger.warn(message)
   end
 
   # @param [String] message
   # @return [void]
   def self.log_error(message)
-    logger.error(message)
+    Celluloid.logger.error(message)
   end
 end
--- test/unit/gitdocs_test.rb
+++ test/unit/gitdocs_test.rb
@@ -3,69 +3,6 @@
 require File.expand_path('../test_helper', __FILE__)
 
 describe Gitdocs do
-  describe 'manager controls' do
-    let(:manager) { mock('Gitdocs::Manager') }
-
-    describe '.start' do
-      subject { Gitdocs.start(:override_web_port) }
-
-      before do
-        Gitdocs::Manager.expects(:new).returns(manager)
-        manager.expects(:start).with(:override_web_port)
-          .returns(:result)
-      end
-
-      it { subject.must_equal(:result) }
-    end
-
-    describe '.restart' do
-      subject { Gitdocs.restart }
-
-      before do
-        Gitdocs.instance_variable_set(:@manager, manager)
-        manager.expects(:restart).returns(:result)
-      end
-
-      it { subject.must_equal(:result) }
-    end
-  end
-
-  describe '.logger' do
-    subject { Gitdocs.logger }
-
-    describe 'already exists' do
-      before { Gitdocs.instance_variable_set(:@logger, :logger) }
-      it { subject.must_equal(:logger) }
-    end
-
-    describe 'does not exist' do
-      let(:logger) { mock('Logger') }
-      before do
-        Gitdocs.instance_variable_set(:@logger, nil)
-
-        Gitdocs.stubs(:log_path).returns(:log_path)
-        Gitdocs::Initializer.stubs(:foreground).returns(foreground)
-        Gitdocs::Initializer.stubs(:verbose).returns(verbose)
-
-        Logger.expects(:new).with(expected_output).returns(logger)
-        logger.expects(:level=).with(expected_level)
-      end
-
-      describe 'no foreground or debug' do
-        let(:foreground) { false } ; let(:verbose) { false } ; let(:expected_output) { :log_path } ; let(:expected_level)  { Logger::INFO }  ; it { subject.must_equal(logger) }
-      end
-      describe 'foreground' do
-        let(:foreground) { false } ; let(:verbose) { true }  ; let(:expected_output) { :log_path } ; let(:expected_level)  { Logger::DEBUG } ; it { subject.must_equal(logger) }
-      end
-      describe 'debug' do
-        let(:foreground) { true }  ; let(:verbose) { false } ; let(:expected_output) { STDOUT }    ; let(:expected_level)  { Logger::INFO }  ; it { subject.must_equal(logger) }
-      end
-      describe 'foreground and debug' do
-        let(:foreground) { true }  ; let(:verbose) { true }  ; let(:expected_output) { STDOUT }    ; let(:expected_level)  { Logger::DEBUG } ; it { subject.must_equal(logger) }
-      end
-    end
-  end
-
   describe '.log_path' do
     subject { Gitdocs.log_path }
     before do
@@ -77,7 +14,7 @@ describe Gitdocs do
 
   describe 'logging wrappers' do
     let(:logger) { mock('Logger') }
-    before { Gitdocs.stubs(:logger).returns(logger) }
+    before { Celluloid.stubs(:logger).returns(logger) }
 
     describe '.log_debug' do
       subject { Gitdocs.log_debug(:message) }
