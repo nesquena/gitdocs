78da237d95c5ac2cb2effcad4eb5421aeb7b5527
Andrew Sullivan Cant <acant@alumni.uwaterloo.ca>
2016-05-26 00:36:18 +0200

    Separate CLI and web tests, only run CLI on Travis
    Since the browser related tests have been the ones that commonly
    fail on timing issues, I am just going to stop running them for now.
    These will still be run, by default, when running the test locally.

---
+++ test/integration/cli/share_management_test.rb
@@ -0,0 +1,29 @@
+# -*- encoding : utf-8 -*-
+
+require File.expand_path('../../test_helper', __FILE__)
+
+describe 'Manage which shares are being watched' do
+  it 'should add a local repository' do
+    gitdocs_add('local')
+    gitdocs_assert_status_contains('local')
+  end
+
+  it 'should add a remote repository' do
+    gitdocs_create_from_remote('local')
+  end
+
+  describe 'remove a share' do
+    before { gitdocs_add('local') }
+    it do
+      gitdocs_command('rm', 'local', 'Removed path local from doc list')
+      gitdocs_assert_status_not_contain('local')
+    end
+  end
+
+  it 'should clear all existing shares' do
+    %w(local1 local2 local3).each { |x| gitdocs_add(x) }
+
+    gitdocs_command('clear', '', 'Cleared paths from gitdocs')
+    gitdocs_assert_status_not_contain('local1', 'local2', 'local3')
+  end
+end
--- test/integration/web/share_management_test.rb
+++ test/integration/web/share_management_test.rb
@@ -1,17 +1,8 @@
 # -*- encoding : utf-8 -*-
 
-require File.expand_path('../test_helper', __FILE__)
+require File.expand_path('../../test_helper', __FILE__)
 
 describe 'Manage which shares are being watched' do
-  it 'should add a local repository' do
-    gitdocs_add('local')
-    gitdocs_assert_status_contains('local')
-  end
-
-  it 'should add a remote repository' do
-    gitdocs_create_from_remote('local')
-  end
-
   it 'should update a share through the UI' do
     gitdocs_add('local')
     gitdocs_start
@@ -41,12 +32,7 @@ describe 'Manage which shares are being watched' do
   describe 'remove a share' do
     before { gitdocs_add('local') }
 
-    it 'through CLI' do
-      gitdocs_command('rm', 'local', 'Removed path local from doc list')
-      gitdocs_assert_status_not_contain('local')
-    end
-
-    it 'through UI' do
+    it do
       gitdocs_start
       visit_and_click_link('Settings')
 
@@ -56,12 +42,5 @@ describe 'Manage which shares are being watched' do
         page.must_have_css('.share', count: 0)
       end
     end
-  end
-
-  it 'should clear all existing shares' do
-    %w(local1 local2 local3).each { |x| gitdocs_add(x) }
-
-    gitdocs_command('clear', '', 'Cleared paths from gitdocs')
-    gitdocs_assert_status_not_contain('local1', 'local2', 'local3')
   end
 end
